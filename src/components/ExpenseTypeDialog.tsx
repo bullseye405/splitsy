42563a39fef10a0a9247fb94cc4e4a752123adeb
Samir Prajapati <bullseye405@gmail.com>
2025-10-08 18:57:05 +0545

    fix transaction design for transaction data

--- src/components/ExpenseTypeDialog.tsx
+++ src/components/ExpenseTypeDialog.tsx
@@ -10,13 +10,6 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { useToast } from '@/hooks/use-toast';
 import { Participant } from '@/types/participants';
